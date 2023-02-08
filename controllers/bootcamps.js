05b8f6f945eb7f48b5f904759752f5f19a89bb21
Xander Mayor <xndrmyr@gmail.com>
2023-02-08 14:28:17 -0500

    Creating our first model

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
@@ -18,6 +20,7 @@ exports.getBootcamp = (req, res, next) => {
 // @route       POST /api/v1/bootcamps
 // @access      Private
 exports.createBootcamp = (req, res, next) => {
+  console.log(req.body);
   res.status(200).json({ success: true, msg: "Create new bootcamps" });
 };
 
