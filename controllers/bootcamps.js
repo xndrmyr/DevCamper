419a26d89cba756dec6b26e9e6ac8e425a2b5488
Xander Mayor <xndrmyr@gmail.com>
2023-02-08 16:19:34 -0500

    feat: fetching bootcamps GET

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,16 +3,30 @@ const Bootcamp = require("../models/Bootcamp");
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Get single bootcamps
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBootcamp = (req, res, next) => {
-  console.log(req.body);
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Create new bootcamp
